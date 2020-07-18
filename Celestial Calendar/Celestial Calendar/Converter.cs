85fd6d732e0a0192b6009507d3be4508d768e18d
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-07-17 22:10:16 -0700

    Added Blazor
    Added Blazor

---
+++ Celestial Calendar/Celestial Calendar/Converter.cs
@@ -0,0 +1,24 @@
+﻿using GaianEmpire;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Converter
+{
+    public DateTime dateTime { get; set; }
+    public StarDate starDate { get; set; }
+    public int year { get; set; }
+    public int month { get; set; }
+    public int day { get; set; }
+
+    public StarDate GetStarDate()
+    {
+        if((this.dateTime.Year == 1) && (this.dateTime.Month == 1) && (this.dateTime.Day == 1))
+        {
+            return new StarDate(new DateTime(1, 1, 1));
+        }
+        else
+        {
+            return new StarDate(this.dateTime);
+        }
+    }
+}
