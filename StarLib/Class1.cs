3c962fa05f95f8227f3f3dd3472c4ad3e66780a1
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-07-23 19:05:04 -0700

    Significantly changed file organization
    Significantly changed file organization

---
+++ StarLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace StarLib
+{
+    public class Class1
+    {
+        //private static bool path;
+
+        public static string FilePath
+        {
+            get
+            {
+                string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string csvFileName = Path.Combine(assemblyFolder, "Languages.csv");
+                return csvFileName;
+            }
+        }
+    }
+}
