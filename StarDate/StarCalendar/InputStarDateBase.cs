ab5ad7325b254556d61288a76bea1ee59d2d939c
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-07-30 20:03:50 -0700

    Finally got element working
    Finally got element working

--- StarDate/StarCalendar/InputStarDateBase.cs
+++ StarDate/StarCalendar/InputStarDateBase.cs
@@ -20,17 +20,189 @@ namespace StarBlaze.StarCalendar
     {
         private const int PlaceHolder = 9999999;
 
-        /// <inheritdoc />
-        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        protected StarDate dt = StarDate.Now;
+        private static List<int> month28;
+        private static List<int> month14;
+        private static List<int> month7;
+        private static Dictionary<int, string> leapList;
+        private static Dictionary<int, string> commonList;
+        public static string test = "Test";
+
+        public int Year { get => dt.Year; set => dt.Year = value; }
+        public int Month { get => dt.Month; set => dt.Month = value; }
+        public int Day { get => dt.Day; set => dt.Day = value; }
+        public int Hour { get => dt.Hour; set => dt.Hour = value; }
+        public int Minute { get => dt.Minute; set => dt.Minute = value; }
+        public int Second { get => dt.Second; set => dt.Second = value; }
+        public string Short { get => dt.ToShortString(); }
+        public string Long { get => dt.ToLongString(); }
+
+        public Dictionary<int, string> MonthList
         {
-            builder.OpenElement(0, "input");
-            builder.AddMultipleAttributes(1, AdditionalAttributes);
-            builder.AddAttribute(2, "type", "date");
-            builder.AddAttribute(3, "class", CssClass);
-            builder.AddAttribute(4, "value", BindConverter.FormatValue(CurrentValueAsString));
-            builder.AddAttribute(5, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
-            builder.CloseElement();
+            get
+            {
+                if (dt.isleapyear())
+                {
+                    return LeapList;
+                }
+                else
+                {
+                    return CommonList;
+                }
+            }
         }
+
+        public List<int> DayList
+        {
+            get
+            {
+                if (Month != 14)
+                {
+                    if (month28 == null)
+                    {
+                        month28 = new List<int>();
+                        int i = 1;
+                        while (i < 29)
+                        {
+                            month28.Add(i++);
+                        }
+                    }
+                    return month28;
+                }
+                else if (dt.isDoubleLeapYear())
+                {
+                    if (month14 == null)
+                    {
+                        month14 = new List<int>();
+                        int i = 1;
+                        while (i < 15)
+                        {
+                            month14.Add(i++);
+                        }
+                    }
+                    return month14;
+                }
+                else
+                {
+                    if (month7 == null)
+                    {
+                        month7 = new List<int>();
+                        int i = 1;
+                        while (i < 8)
+                        {
+                            month7.Add(i++);
+                        }
+                    }
+                    return month7;
+                }
+            }
+        }
+
+        public int Horus
+        {
+            get
+            {
+                if (dt.isDoubleLeapYear())
+                {
+                    return 14;
+                }
+                else if (dt.isleapyear())
+                {
+                    return 7;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public Dictionary<int, string> CommonList
+        {
+            get
+            {
+                if (commonList == null)
+                {
+                    GenerateLists();
+                }
+                return commonList;
+            }
+        }
+
+        public Dictionary<int, string> LeapList
+        {
+            get
+            {
+                if (leapList == null)
+                {
+                    GenerateLists();
+                }
+                return leapList;
+            }
+        }
+
+        private void GenerateLists()
+        {
+            leapList = new Dictionary<int, string>();
+            commonList = new Dictionary<int, string>();
+            int i = 0;
+            int j = 13;
+            while (i < j)
+            {
+                string s = StarCulture.MonthSymbols[i] + " " + StarCulture.CurrentCulture.GetMonthNameFromIndex(i);
+                commonList.Add(i + 1, s);
+                leapList.Add(i + 1, s);
+                i++;
+            }
+            leapList.Add(14, StarCulture.MonthSymbols[13] + " " + StarCulture.CurrentCulture.GetMonthNameFromIndex(13));
+        }
+
+        public override string ToString()
+        {
+            return dt;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Legacy
+        /// </summary>
+        /// <param name="builder"></param>
+        //protected override void BuildRenderTree(RenderTreeBuilder builder)
+        //{
+        //    builder.OpenElement(0, "input");
+        //    builder.AddMultipleAttributes(1, AdditionalAttributes);
+        //    builder.AddAttribute(2, "type", "date");
+        //    builder.AddAttribute(3, "class", CssClass);
+        //    builder.AddAttribute(4, "value", BindConverter.FormatValue(CurrentValueAsString));
+        //    builder.AddAttribute(5, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
+        //    builder.CloseElement();
+        //}
 
         protected override bool TryParseValueFromString(string value, out TValue result, out string validationErrorMessage)
         {
