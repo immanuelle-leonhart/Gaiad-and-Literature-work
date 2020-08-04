d0b5130405bd9dcd9fa9196855f2a2617dfd2298
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-08-04 14:14:39 -0700

    Found a weird error
    @bind works outside class library but not in class library it seems

--- StarLibForms/InputStarDateBase.cs
+++ StarLibForms/InputStarDateBase.cs
@@ -11,14 +11,15 @@ namespace StarLib.Forms
 {
     public class InputStarDateBase : InputBase<StarDate>
     {
-        private int day;
-        private int month;
-        private int year;
+        protected int day;
+        protected int month;
+        protected int year;
         public bool presentbound = false;
 
         //[Parameter] public new StarLib.StarDate Value { get => sdt; set => sdt = value; }
 
-        public int MonthDays { get; private set; }
+        public int MonthDays { get; protected set; }
+        public int YearMonths { get; protected set; }
 
         protected StarDate Foundation
         {
@@ -34,51 +35,9 @@ namespace StarLib.Forms
                 }
             }
         }
-        public int YearMonths { get; private set; }
+        
 
         public string[] Months { get; set; } = StarLib.StarCulture.CurrentCulture.GetMonthList();
-
-        protected override void OnInitialized()
-        {
-            Value = StarLib.StarDate.Now;
-            year = Value.Year;
-            month = Value.Month;
-            day = Value.Day;
-            MonthDays = Value.GetMonthDays();
-            YearMonths = Value.GetYearMonths();
-        }
-
-        /// <inheritdoc />
-        protected override bool TryParseValueFromString(string value, [MaybeNull] out StarDate result, [NotNullWhen(false)] out string validationErrorMessage)
-        {
-            // Unwrap nullable types. We don't have to deal with receiving empty values for nullable
-            // types here, because the underlying InputBase already covers that.
-            try
-            {
-                result = StarDate.fromQuickString(value);
-                validationErrorMessage = "Success";
-                return true;
-            }
-            catch (Exception)
-            {
-                result = default;
-                validationErrorMessage = "Invalid value";
-                return false;
-            }
-        }
-
-        /// <inheritdoc />
-        protected override string FormatValueAsString([AllowNull] StarDate value)
-        {
-            if (value == null)
-            {
-                throw new ArgumentNullException();
-            }
-            else
-            {
-                return value.QuickString();
-            }
-        }
 
         public int Day
         {
@@ -122,5 +81,49 @@ namespace StarLib.Forms
                 YearMonths = Value.GetYearMonths();
             }
         }
+
+        protected override void OnInitialized()
+        {
+            Value = StarLib.StarDate.Now;
+            year = Value.Year;
+            month = Value.Month;
+            day = Value.Day;
+            MonthDays = Value.GetMonthDays();
+            YearMonths = Value.GetYearMonths();
+        }
+
+        /// <inheritdoc />
+        protected override bool TryParseValueFromString(string value, [MaybeNull] out StarDate result, [NotNullWhen(false)] out string validationErrorMessage)
+        {
+            // Unwrap nullable types. We don't have to deal with receiving empty values for nullable
+            // types here, because the underlying InputBase already covers that.
+            try
+            {
+                result = StarDate.fromQuickString(value);
+                validationErrorMessage = "Success";
+                return true;
+            }
+            catch (Exception)
+            {
+                result = default;
+                validationErrorMessage = "Invalid value";
+                return false;
+            }
+        }
+
+        /// <inheritdoc />
+        protected override string FormatValueAsString([AllowNull] StarDate value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return value.QuickString();
+            }
+        }
+
+        
     }
 }
