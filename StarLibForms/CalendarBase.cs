d64cb8af1ac4597331b9c9eb0e5e5d04dc38f414
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-08-04 16:19:19 -0700

    Added Calendar Razor Components
    Added Calendar Razor Components

---
+++ StarLibForms/CalendarBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarLib.Forms
+{
+    public class CalendarBase : ComponentBase
+    {
+    }
+}
