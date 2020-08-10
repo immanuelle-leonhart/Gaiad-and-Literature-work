930ee10513f34b1b7c4ee1164fc7820d0f64fbcf
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-08-10 16:50:12 -0700

    Finished Dynamic
    Finished Dynamic

---
+++ StarLib.Dynamic/StarZoneDynamic.cs
@@ -0,0 +1,13 @@
+﻿using StarLib;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace StarLib.Dynamic
+{
+    class StarZoneDynamic : DynamicObject
+    {
+        StarZone timezone;
+    }
+}
