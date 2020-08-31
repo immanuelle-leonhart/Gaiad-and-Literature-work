90eea1112434a9a4951ce9f991ab43f49f7d3a33
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-08-30 21:57:16 -0700

    added important link
    added important link

--- StarLib.Dynamic/StarCultureDynamic.cs
+++ StarLib.Dynamic/StarCultureDynamic.cs
@@ -4,6 +4,10 @@ using System.Dynamic;
 using System.Text;
 using StarLib;
 
+/// <summary>
+/// https://www.red-gate.com/simple-talk/dotnet/net-framework/dynamic-language-integration-in-a-c-world/
+/// </summary>
+
 namespace StarLib.Dynamic
 {
     class StarCultureDynamic : DynamicObject
