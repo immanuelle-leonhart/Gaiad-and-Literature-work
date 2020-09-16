13d623cb45e634ab8cc35a97b044d2229d786d73
siliconprophet <49389064+siliconprophet@users.noreply.github.com>
2020-09-16 15:47:43 -0700

    y
    y

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(StarDate.Now);
-            Console.WriteLine(Relative.GetRelationship("F"));
+            string path = "C:/Users/SiliconProphet/Documents/GitHub/Order/StarDate/ICAL/calendar.ical";
+            string path2 = "C:/Users/SiliconProphet/Documents/GitHub/Order/StarDate/ICAL/";
+            string[] p = path.Split('/');
+            string[] p2 = path2.Split('/');
+            foreach (string entry in p2)
+            {
+                Console.WriteLine("line: " + entry + " length " + entry.Length);
+            }
+            foreach (string entry in p)
+            {
+                Console.WriteLine("line: " + entry);
+            }
+            StarDate.WriteIcal("C:/Users/SiliconProphet/Documents/GitHub/Order/StarDate/ICAL");
+            StarDate.WriteIcal("C:/Users/SiliconProphet/Documents/GitHub/Order/StarDate/ICAL", 2020);
+            StarDate.WriteIcal("C:/Users/SiliconProphet/Documents/GitHub/Order/StarDate/ICAL", 2010, 2020);
         }
     }
 }
